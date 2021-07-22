f582d531cb88aa6cc30e9090284c18695804832e
Mohamed-Naufal <mohamednaufalr@gmail.com>
2021-07-22 11:45:34 +0530

    5th commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const users = fs.readFileSync('./users.json')
+const usersData = JSON.parse(users);
+
 app.get('/' , (req, res) => {
     res.send('Welcome to the Backend World....')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.get('/test' , (req, res) => {
