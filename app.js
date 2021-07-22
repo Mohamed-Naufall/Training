4891ae8c8cf0cc4156d026f2c34da1d8b347e91a
Mohamed-Naufal <mohamednaufalr@gmail.com>
2021-07-22 12:48:57 +0530

    11th commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-const users = fs.readFileSync('./users.json')
+const users = fs.readFileSync('./tours.json')
 const usersData = JSON.parse(users);
 
 app.get('/' , (req, res) => {
     res.send('Welcome to the Backend World....')
 })
 
-app.get('/users', (req, res) => {
-
+app.get('/tours', (req, res) => {
 
     res.status(200).json({
         status: 'success',
@@ -25,13 +26,48 @@ app.get('/test' , (req, res) => {
     res.send('This is the test api....')
 })
 
+app.get('/api',(req, res) => {
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            usersData
+        }
+    });
+})
+
+
+app.get('/api/:id',(req, res) => {
+    id = req.params.id * 2;
+    const tour = usersData.find(el => el.id === id)
+    if (id > tour.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status:'success',
+        data: {
+            tour
+        }
+    })
+
+})
+
 app.get('*', (req, res) => {
     res.status(404).json({
         status: 'failure', 
-        data: {
-            message: `You've entered the invalid path....`
-        }     
+        data: { 
+        },
+        message: `You've entered the wrong path...`   
     })
+})
+
+
+
+app.get('/api/:id', () => {
+    console.log(req.params.id);
 })
 
 app.listen(port, () => {
