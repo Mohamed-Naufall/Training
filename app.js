ea9162f0bea89ee9485de0658ca2bbee689b600b
Mohamed-Naufal <mohamednaufalr@gmail.com>
2021-07-22 11:10:58 +0530

    second commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/' , (req, res) => {
     res.send('Welcome to the Backend World....')
 })
 
+app.get('/test' , (req, res) => {
+    res.send('Testing')
+})
+
 app.listen(port, () => {
     console.log(`Server is listening to the port ${port}....`);
 })
